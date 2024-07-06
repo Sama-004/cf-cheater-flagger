f40583d2f4618c57e9665f2d90b5c3cebfb62535
sama-004 <70210929+Sama-004@users.noreply.github.com>
2024-07-06 20:05:35 +0530

    cheater header + whole bg + userbox

--- content.js
+++ content.js
@@ -17,6 +17,23 @@ fetch(url)
     if (count > 0) {
       console.log("Style implemented");
       document.body.style.backgroundColor = "#ff0000";
+      const header = document.createElement("h1");
+      //header.textContent = `Skipped Verdicts: ${count}`;
+      header.textContent = `Cheater`;
+      header.style.color = "#ffffff"; // Example: white text color
+      header.style.position = "fixed";
+      header.style.top = "0";
+      header.style.left = "0";
+      header.style.width = "100%";
+      header.style.marginBottom = "10px";
+      header.style.backgroundColor = "#333"; // Example background color
+      document.body.insertBefore(header, document.body.firstChild);
+      var elements = document.getElementsByClassName("userbox");
+      for (var i = 0; i < elements.length; i++) {
+        elements[i].style.backgroundColor = "#ff0000";
+      }
+
+      //document.body.appendChild(header);
     }
   })
   .catch((error) => {
