e421d562b332f64d3bd608b18e1beb275d16bfb1
sama-004 <70210929+Sama-004@users.noreply.github.com>
2024-07-06 19:15:23 +0530

    skipped number to console for single user

--- background.js
+++ background.js
@@ -1,24 +1,7 @@
-//const url = 'https://codeforces.com/api/user.status?handle=sama004';
 chrome.action.onClicked.addListener((tab) => {
-const url = 'https://codeforces.com/api/user.status?handle=Sajal_singh24';
-
-let count=0;
-fetch(url)
-  .then(response => response.json())
-  .then(data => {
-    if (data.status === 'OK') {
-      data.result.forEach(entry => {
-        if (entry.verdict === 'SKIPPED') {
-            count++;
-          //console.log(entry);
-        }
-      });
-    } else {
-      console.error('Error fetching data:', data.comment);
-    }
-      console.log(count)
-  })
-  .catch(error => {
-    console.error('Error:', error);
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    files: ['content.js']
   });
-})
+});
+
