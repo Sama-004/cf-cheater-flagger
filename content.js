ee045cdebe1d774f0da03ea92f31de1399268cbd
sama-004 <70210929+Sama-004@users.noreply.github.com>
2024-07-06 22:28:14 +0530

    rank to cheater

--- content.js
+++ content.js
@@ -17,9 +17,16 @@ fetch(url)
     if (count > 0) {
       //console.log("Style implemented");
       //document.body.style.backgroundColor = "#ff0000";
-      var elements = document.getElementsByClassName("userbox");
+      //var elements = document.getElementsByClassName("userbox");
+      let elements = document.getElementsByClassName("roundbox"); //this looks much better(take others opinion before using or maybe add options to the user itself to select what he wants)
+      //also give user the freedom to select/change color from extension options
       for (let i = 0; i < elements.length; i++) {
         elements[i].style.backgroundColor = "#ff0000";
+        //#d4312c
+      }
+      let title = document.getElementsByClassName("user-rank");
+      for (let i = 0; i < title.length; i++) {
+        title[i].innerHTML = "Cheater";
       }
     }
   })
