65ef219eb7cc8fcef2d85f64e2feea3412ab58ad
sama-004 <70210929+Sama-004@users.noreply.github.com>
2024-07-06 19:57:22 +0530

    bg change if skipped

--- content.js
+++ content.js
@@ -1,22 +1,24 @@
-const username = window.location.pathname.split('/').pop();
+const username = window.location.pathname.split("/").pop();
 const url = `https://codeforces.com/api/user.status?handle=${username}`;
 
-let count=0;
+let count = 0;
 fetch(url)
-  .then(response => response.json())
-  .then(data => {
-    if (data.status === 'OK') {
-      data.result.forEach(entry => {
-        if (entry.verdict === 'SKIPPED') {
-            count++;
-          //console.log(entry);
+  .then((response) => response.json())
+  .then((data) => {
+    if (data.status === "OK") {
+      data.result.forEach((entry) => {
+        if (entry.verdict === "SKIPPED") {
+          count++;
         }
       });
     } else {
-      console.error('Error fetching data:', data.comment);
+      console.error("Error fetching data:", data.comment);
     }
-      console.log(count)
+    if (count > 0) {
+      console.log("Style implemented");
+      document.body.style.backgroundColor = "#ff0000";
+    }
   })
-  .catch(error => {
-    console.error('Error:', error);
+  .catch((error) => {
+    console.error("Error:", error);
   });
